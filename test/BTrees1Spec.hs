7b83573f15a7e1ec87735b16fd00ee6de272e088
Gustavo Franke <franke.gustavo@gmail.com>
2020-10-13 09:05:14 +0100

    add btree 1, and its spec

---
+++ test/BTrees1Spec.hs
@@ -0,0 +1,18 @@
+module BTrees1Spec where
+
+import BTrees1
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Problem 56" $ do
+
+    it "can symmetric" $ do
+      symmetric (Branch 'x' (Branch 'x' Empty Empty) Empty) `shouldBe` False
+      symmetric (Branch 'x' (Branch 'x' Empty Empty) (Branch 'x' Empty Empty)) `shouldBe` True
+
+  describe "Problem 57" $ do
+
+    it "can symmetric" $ do
+      (symmetric . construct $ [5, 3, 18, 1, 4, 12, 21]) `shouldBe` True
+      (symmetric . construct $ [3, 2, 5, 7, 1]) `shouldBe` True
