1055abf2d3130263199c897e3f4cdd5465d28f11
Gustavo Franke <franke.gustavo@gmail.com>
2020-10-16 09:08:46 +0100

    improve docs for list 3, arith, logic

--- src/Logic.hs
+++ src/Logic.hs
@@ -1,33 +1,35 @@
+-- | Questions 46 to 50: Logic and codes
+-- | Logic and Codes
 module Logic where
 
 import Data.List
 
--- | Questions 46 to 50: Logic and codes
--- | Logic and Codes
+{-|
+ Problem 46
 
--- Problem 46
--- (**) Define predicates and/2, or/2, nand/2, nor/2, xor/2, impl/2 and equ/2 (for logical equivalence)
---  which succeed or fail according to the result of their respective operations;
---   e.g. and(A,B) will succeed, if and only if both A and B succeed.
-
--- A logical expression in two variables can then be written as in the following example: and(or(A,B),nand(A,B)).
-
--- Now, write a predicate table/3 which prints the truth table of a given logical expression in two variables.
-
--- Example:
-
--- (table A B (and A (or A B)))
--- true true true
--- true fail true
--- fail true fail
--- fail fail fail
--- Example in Haskell:
-
--- λ> table (\a b -> (and' a (or' a b)))
--- True True True
--- True False True
--- False True False
--- False False False
+ (**) Define predicates and/2, or/2, nand/2, nor/2, xor/2, impl/2 and equ/2 (for logical equivalence)
+  which succeed or fail according to the result of their respective operations;
+   e.g. and(A,B) will succeed, if and only if both A and B succeed.
+ 
+ A logical expression in two variables can then be written as in the following example: and(or(A,B),nand(A,B)).
+ 
+ Now, write a predicate table/3 which prints the truth table of a given logical expression in two variables.
+ 
+ Example:
+ 
+ (table A B (and A (or A B)))
+ true true true
+ true fail true
+ fail true fail
+ fail fail fail
+ Example in Haskell:
+ 
+ >>> table (\a b -> (and' a (or' a b)))
+ True True True
+ True False True
+ False True False
+ False False False
+-}
 and' :: Bool -> Bool -> Bool
 and' a b
     | (a, b) == (True, True) = True
@@ -68,74 +70,82 @@ table f = map (\x -> (fst x) : (snd x) : f (fst x) (snd x) : []) inputs
 -- > table (\a b -> (and' a (or' a b)))
 -- [[True,True,True],[True,False,True],[False,True,False],[False,False,False]]
 
--- Problem 47
--- (*) Truth tables for logical expressions (2).
+{-|
+ Problem 47
 
--- Continue problem P46 by defining and/2, or/2, etc as being operators.
--- This allows to write the logical expression in the more natural way, as in the example: A and (A or not B).
--- Define operator precedence as usual; i.e. as in Java.
-
--- Example:
-
--- * (table A B (A and (A or not B)))
--- true true true
--- true fail true
--- fail true fail
--- fail fail fail
--- Example in Haskell:
-
--- λ> table2 (\a b -> a `and'` (a `or'` not b))
--- True True True
--- True False True
--- False True False
--- False False False
-
--- table (\a b -> a `and'` (a `or'` not b))
--- [[True,True,True],[True,False,True],[False,True,False],[False,False,False]]
--- TODO: I think there's no impl needed here?
+ (*) Truth tables for logical expressions (2).
+ 
+ Continue problem P46 by defining and/2, or/2, etc as being operators.
+ This allows to write the logical expression in the more natural way, as in the example: A and (A or not B).
+ Define operator precedence as usual; i.e. as in Java.
+ 
+ Example:
+ 
+ * (table A B (A and (A or not B)))
+ true true true
+ true fail true
+ fail true fail
+ fail fail fail
+ Example in Haskell:
+ 
+ >>> table2 (\a b -> a `and'` (a `or'` not b))
+ True True True
+ True False True
+ False True False
+ False False False
+ 
+ table (\a b -> a `and'` (a `or'` not b))
+ [[True,True,True],[True,False,True],[False,True,False],[False,False,False]]
+ TODO: I think there's no impl needed here?
 -------------------------------
+-}
 
--- Problem 48
--- (**) Truth tables for logical expressions (3).
+{-|
+ Problem 48
 
--- Generalize problem P47 in such a way that the logical expression may contain any number of logical variables.
--- Define table/2 in a way that table(List,Expr) prints the truth table for the expression Expr,
--- which contains the logical variables enumerated in List.
+ (**) Truth tables for logical expressions (3).
+ 
+ Generalize problem P47 in such a way that the logical expression may contain any number of logical variables.
+ Define table/2 in a way that table(List,Expr) prints the truth table for the expression Expr,
+ which contains the logical variables enumerated in List.
+ 
+ Example:
 
--- Example:
-
--- * (table (A,B,C) (A and (B or C) equ A and B or A and C))
--- true true true true
--- true true fail true
--- true fail true true
--- true fail fail true
--- fail true true true
--- fail true fail true
--- fail fail true true
--- fail fail fail true
--- Example in Haskell:
-
--- λ> tablen 3 (\[a,b,c] -> a `and'` (b `or'` c) `equ'` a `and'` b `or'` a `and'` c)
--- -- infixl 3 `equ'`
--- True  True  True  True
--- True  True  False True
--- True  False True  True
--- True  False False True
--- False True  True  True
--- False True  False True
--- False False True  True
--- False False False True
-
--- -- infixl 7 `equ'`
--- True  True  True  True
--- True  True  False True
--- True  False True  True
--- True  False False False
--- False True  True  False
--- False True  False False
--- False False True  False
--- False False False False
-
+ @
+ * (table (A,B,C) (A and (B or C) equ A and B or A and C))
+ true true true true
+ true true fail true
+ true fail true true
+ true fail fail true
+ fail true true true
+ fail true fail true
+ fail fail true true
+ fail fail fail true
+ Example in Haskell:
+ 
+ >>> tablen 3 (\[a,b,c] -> a `and'` (b `or'` c) `equ'` a `and'` b `or'` a `and'` c)
+  infixl 3 `equ'`
+ True  True  True  True
+ True  True  False True
+ True  False True  True
+ True  False False True
+ False True  True  True
+ False True  False True
+ False False True  True
+ False False False True
+ 
+  infixl 7 `equ'`
+ True  True  True  True
+ True  True  False True
+ True  False True  True
+ True  False False False
+ False True  True  False
+ False True  False False
+ False False True  False
+ False False False False
+ @
+ 
+-}
 tablen :: Integral b => b -> ([Bool] -> Bool) -> [[Bool]]
 tablen n f = map (\i -> i ++ [f i]) inputs
     where colums = [1 .. n]
@@ -186,23 +196,27 @@ tablen n f = map (\i -> i ++ [f i]) inputs
 -- *Main> transpose [[True,True,True,True,False,False,False,False],[True,True,False,False,True,True,False,False],[True,False,True,False,True,False,True,False]]
 -- [[True,True,True],[True,True,False],[True,False,True],[True,False,False],[False,True,True],[False,True,False],[False,False,True],[False,False,False]]
 -------------------------------
--- Problem 49
--- (**) Gray codes.
 
--- An n-bit Gray code is a sequence of n-bit strings constructed according to certain rules. For example,
+{-|
+ Problem 49
 
--- n = 1: C(1) = ['0','1'].
--- n = 2: C(2) = ['00','01','11','10'].
--- n = 3: C(3) = ['000','001','011','010',´110´,´111´,´101´,´100´].
--- Find out the construction rules and write a predicate with the following specification:
-
--- % gray(N,C) :- C is the N-bit Gray code
--- Can you apply the method of "result caching" in order to make the predicate more efficient, when it is to be used repeatedly?
-
--- Example in Haskell:
-
--- λ> gray 3
--- ["000","001","011","010","110","111","101","100"]
+ (**) Gray codes.
+ 
+ An n-bit Gray code is a sequence of n-bit strings constructed according to certain rules. For example,
+ 
+ n = 1: C(1) = ['0','1'].
+ n = 2: C(2) = ['00','01','11','10'].
+ n = 3: C(3) = ['000','001','011','010',´110´,´111´,´101´,´100´].
+ Find out the construction rules and write a predicate with the following specification:
+ 
+ % gray(N,C) :- C is the N-bit Gray code
+ Can you apply the method of "result caching" in order to make the predicate more efficient, when it is to be used repeatedly?
+ 
+ Example in Haskell:
+ 
+ >>> gray 3
+ ["000","001","011","010","110","111","101","100"]
+-}
 gray :: Integral b => b -> [[Char]]
 gray n = reverse $ transpose (map (\x -> take (2 ^ n) (cycle (trues x ++ falses x))) $ reverse colums)
     where colums = [1 .. n]
@@ -210,23 +224,30 @@ gray n = reverse $ transpose (map (\x -> take (2 ^ n) (cycle (trues x ++ falses 
           trues  = \x -> bools x '1'
           falses = \x -> bools x '0'
 
--- Problem 50
--- (***) Huffman codes.
+{-|
+ Problem 50
 
--- We suppose a set of symbols with their frequencies, given as a list of fr(S,F) terms.
--- Example: [fr(a,45),fr(b,13),fr(c,12),fr(d,16),fr(e,9),fr(f,5)].
--- Our objective is to construct a list hc(S,C) terms, where C is the Huffman code word for the symbol S.
--- In our example, the result could be Hs = [hc(a,'0'), hc(b,'101'), hc(c,'100'), hc(d,'111'), hc(e,'1101'), hc(f,'1100')] [hc(a,'01'),...etc.].
--- The task shall be performed by the predicate huffman/2 defined as follows:
+ (***) Huffman codes.
+ 
+ We suppose a set of symbols with their frequencies, given as a list of fr(S,F) terms.
 
--- % huffman(Fs,Hs) :- Hs is the Huffman code table for the frequency table Fs
--- Example in Haskell:
+ Example: [fr(a,45),fr(b,13),fr(c,12),fr(d,16),fr(e,9),fr(f,5)].
 
--- λ> huffman [('a',45),('b',13),('c',12),('d',16),('e',9),('f',5)]
--- [('a',"0"),('b',"101"),('c',"100"),('d',"111"),('e',"1101"),('f',"1100")]
+ Our objective is to construct a list hc(S,C) terms, where C is the Huffman code word for the symbol S.
+ In our example, the result could be Hs = [hc(a,'0'), hc(b,'101'), hc(c,'100'), hc(d,'111'), hc(e,'1101'), hc(f,'1100')] [hc(a,'01'),...etc.].
+ The task shall be performed by the predicate huffman/2 defined as follows:
+ 
+ % huffman(Fs,Hs) :- Hs is the Huffman code table for the frequency table Fs
+ 
+ Example in Haskell:
+ 
+ >>> huffman [('a',45),('b',13),('c',12),('d',16),('e',9),('f',5)]
+ [('a',"0"),('b',"101"),('c',"100"),('d',"111"),('e',"1101"),('f',"1100")]
+ 
 
--- TODO: below is what I have so far
--- *Main GHC.Integer.Logarithms> map ((/ 100) . snd) [('a',45),('b',13),('c',12),('d',16),('e',9),('f',5)]
--- [0.45,0.13,0.12,0.16,9.0e-2,5.0e-2]
--- *Main GHC.Integer.Logarithms> sum $ map ((/ 100) . snd) [('a',45),('b',13),('c',12),('d',16),('e',9),('f',5)]
--- 1.0
+ TODO: below is what I have so far
+ *Main GHC.Integer.Logarithms> map ((/ 100) . snd) [('a',45),('b',13),('c',12),('d',16),('e',9),('f',5)]
+ [0.45,0.13,0.12,0.16,9.0e-2,5.0e-2]
+ *Main GHC.Integer.Logarithms> sum $ map ((/ 100) . snd) [('a',45),('b',13),('c',12),('d',16),('e',9),('f',5)]
+ 1.0
+-}
--- test/LogicSpec.hs
+++ test/LogicSpec.hs
@@ -2,6 +2,7 @@ module LogicSpec where
 
 import Logic
 import Test.Hspec
+import qualified Data.Set as Set
 
 spec :: Spec
 spec = do
@@ -10,3 +11,8 @@ spec = do
     it "can table" $ do
       table (\a b -> (and' a (or' a b))) `shouldBe` [[True,True,True],[True,False,True],[False,True,False],[False,False,False]]
       table (\a b -> a `and'` (a `or'` not b)) `shouldBe` [[True,True,True],[True,False,True],[False,True,False],[False,False,False]]
+
+  describe "Problem 49" $ do
+
+    it "can gray 3" $ do
+      (Set.fromList $ gray (3::Int)) `shouldBe` Set.fromList ["000","001","011","010","110","111","101","100"]
